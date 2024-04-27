d073f85330acd18e1376a961d4aafa866df4cef4
Maxime Daniel <root@maxux.net>
2024-04-27 16:24:23 +0200

    location: fetch from serial and forward

---
+++ backend/location.py
@@ -0,0 +1,84 @@
+import serial
+import tools.nmea0183
+import dashboard
+import redis
+import json
+import math
+
+class MagibuxLocator:
+    def __init__(self, port):
+        self.gps = serial.Serial(port, 9600)
+        self.parser = tools.nmea0183.GPSData()
+        self.places = redis.Redis()
+
+        self.slave = dashboard.DashboardSlave("location")
+        self.previous = None
+        self.trip = 0
+
+    def distance(self, lat1, lon1, lat2, lon2):
+        # haversine // https://www.movable-type.co.uk/scripts/latlong.html
+        R = 6371000
+
+        x1 = lat1 * math.pi / 180
+        x2 = lat2 * math.pi / 180
+        dp = (lat2 - lat1) * math.pi / 180
+        da = (lon2 - lon1) * math.pi / 180
+
+        a = math.sin(dp / 2) * math.sin(dp / 2)
+        a += math.cos(x1) * math.cos(x2) * math.sin(da / 2) * math.sin(da / 2)
+
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+        return float("%.2f" % (R * c))
+
+    def place(self, lat, lon):
+        listbase = self.places.georadius("geonames.main.base", lon, lat, 5, "km", "withdist", "asc")
+        basedata = self.places.hget("geonames.data", listbase[0][0])
+
+        base = json.loads(basedata)
+
+        cdata = self.places.hget("geonames.countries", base[1])
+        cinfo = json.loads(cdata)
+
+        admin1 = self.places.hget("geonames.admin1", f"{base[1]}.{base[2]}").decode("utf-8")
+        admin2 = self.places.hget("geonames.admin2", f"{base[1]}.{base[2]}.{base[3]}").decode("utf-8")
+
+        return [base[0], admin2, admin1, cinfo["name"]]
+
+    def loop(self):
+        line = self.gps.readline()
+        data = line.decode('utf-8').strip()
+
+        parsed = self.parser.parse(data)
+        print(parsed)
+
+        if parsed['type'] != 'rmc':
+            # only process GPRMC data for now
+            return
+
+        coord = parsed['coord']
+        parsed['place'] = self.place(coord['lat'], coord['lng'])
+
+        if not self.previous:
+            self.previous = coord
+
+        # compute distance from last location
+        distance = self.distance(coord['lat'], coord['lng'], self.previous['lat'], self.previous['lng'])
+        # FIXME: skip if not enough
+        parsed['delta'] = distance
+
+        self.trip += distance
+        parsed['trip'] = self.trip
+
+        self.slave.set(parsed)
+        self.slave.publish()
+
+        self.previous = coord
+
+    def monitor(self):
+        while True:
+            self.loop()
+
+if __name__ == "__main__":
+    locator = MagibuxLocator("/dev/ttyACM0")
+    locator.monitor()
---
+++ backend/tools/nmea0183.py
@@ -0,0 +1,170 @@
+import json
+import sys
+from datetime import datetime
+from datetime import timezone
+
+class GPSRawData:
+    def checksum(self, line):
+        # malformed line
+        if not line.startswith('$') or line[-3] != '*':
+            return False
+
+        control = int("0x%s" % line[-2:], 16)
+        compare = line[1:-3]
+        compute = 0
+
+        for entry in compare:
+            compute ^= ord(entry)
+
+        return (control == compute)
+
+    def _time(self, tf):
+        return "%s:%s:%s" % (tf[0:2], tf[2:4], tf[4:6])
+
+    def _date(self, df):
+        return '20%s-%s-%s' % (df[4:6], df[2:4], df[0:2])
+
+    def _dmsdd(self, lat, latcard, lon, loncard):
+        if not lat:
+            return {'lat': None, 'lng': None}
+
+        xlat = {'d': int(lat[0:2]), 'ms': float(lat[2:])}
+        xlon = {'d': int(lon[0:3]), 'ms': float(lon[3:])}
+
+        ddlat = xlat['d'] + (xlat['ms'] / 60)
+        ddlon = xlon['d'] + (xlon['ms'] / 60)
+
+        if latcard == 'S':
+            ddlat = -ddlat
+
+        if loncard == 'W':
+            ddlon = -ddlon
+
+        return {'lat': ddlat, 'lng': ddlon, 'rlat': lat, 'rlng': lon}
+
+    def _quality(self, quality):
+        if quality == '0':
+            return 'invalid'
+
+        if quality == '1':
+            return 'GPS'
+
+        if quality == '2':
+            return 'DGPS'
+
+        return 'unknown'
+
+    # Recommended minimum specific GPS/Transit data
+    def gprmc(self, fields):
+        time = self._time(fields[1])
+        date = self._date(fields[9])
+
+        dt = datetime.strptime('%s %s' % (date, time), '%Y-%m-%d %H:%M:%S')
+        timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
+
+        coord = self._dmsdd(fields[3], fields[4], fields[5], fields[6])
+        warning = fields[2] == "A"
+
+        # knots to kph
+        speed = float(fields[7]) * 1.852
+
+        return {
+            'type': 'rmc',
+            'timestamp': int(timestamp),
+            'time': time,
+            'date': date,
+            'coord': coord,
+            'speed': speed,
+            'validity': (fields[2] == "A"),
+        }
+
+    # GPS Satellites in view
+    def gpgsv(self, fields):
+        return {'type': 'gsv'}
+
+    def gpgll(self, fields):
+        return {'type': 'gll'}
+
+    # Track made good and ground speed
+    def gpvtg(self, fields):
+        track = float(fields[1]) if fields[1] else None
+        speed = float(fields[7]) if fields[7] else None
+
+        if speed > 260:
+            speed = 0
+
+        data = {
+            'type': 'vtg',
+            'track': track,
+            'speed': speed
+        }
+
+        return data
+
+    # GPS DOP and active satellites
+    def gpgsa(self, fields):
+        mode = 'automatic' if fields[1] == 'A' else 'manual'
+        fix = fields[2] + 'd' if fields[2] != '1' else 'not available'
+
+        return {'type': 'gsa', 'mode': mode, 'fix': fix}
+
+    # Global Positioning System Fix Data
+    def gpgga(self, fields):
+        time = self._time(fields[1])
+        coord = self._dmsdd(fields[2], fields[3], fields[4], fields[5])
+        quality = self._quality(fields[6])
+        hdop = float(fields[8]) if fields[8] else 0
+        altitude = float(fields[9]) if fields[9] else 0
+
+        return {
+            'type': 'gga',
+            'time': time,
+            'coord': coord,
+            'quality': quality,
+            'sats': int(fields[7]),
+            'hdop': hdop,       # Horizontal Dilution of Precision
+            'altitude': altitude
+        }
+
+    def gptxt(self, fields):
+        return {"type": "info", "text": fields[4]}
+
+class GPSData:
+    def __init__(self):
+        self.raw = GPSRawData()
+
+    def parse(self, line):
+        print(line)
+
+        if not self.raw.checksum(line):
+            return {'type': 'bad checksum'}
+
+        # strip checksum
+        line = line[:-3]
+
+        fields = line.split(',')
+        action = fields[0]
+        # print(fields)
+
+        if action == "$GPRMC":
+            return self.raw.gprmc(fields)
+
+        if action == "$GPGSV":
+            return self.raw.gpgsv(fields)
+
+        if action == "$GPVTG":
+            return self.raw.gpvtg(fields)
+
+        if action == "$GPGSA":
+            return self.raw.gpgsa(fields)
+
+        if action == "$GPGGA":
+            return self.raw.gpgga(fields)
+
+        if action == "$GPGLL":
+            return self.raw.gpgll(fields)
+
+        if action == "$GPTXT":
+            return self.raw.gptxt(fields)
+
+        return {'type': 'unknown'}
