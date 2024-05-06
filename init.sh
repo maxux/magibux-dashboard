b90e817df6d970a417621630d1112cb970e1707f
Maxime Daniel <root@maxux.net>
2024-05-06 07:11:10 +0200

    init: move http port to default 80

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tmux new-sess -s dash-http -d 'cd /root/dashboard/frontend && python -m http.server'
+tmux new-sess -s dash-http -d 'cd /root/dashboard/frontend && python -m http.server 80'
 tmux new-sess -s dash-main -d 'cd /root/dashboard/backend && python dashboard.py'
 tmux new-sess -s locator -d 'cd /root/dashboard/backend && python location.py'
