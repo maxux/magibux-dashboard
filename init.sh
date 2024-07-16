3d899c3a65bec64a0f0aa5423c808dea20d73d86
Maxime Daniel <root@maxux.net>
2024-07-16 11:29:36 +0200

    init: include new services and improve syntax

--- init.sh
+++ init.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-tmux new-sess -s dash-http -d 'cd /root/dashboard/frontend && python -m http.server 80'
-tmux new-sess -s dash-main -d 'cd /root/dashboard/backend && python dashboard.py'
-tmux new-sess -s locator -d 'cd /root/dashboard/backend && python location.py'
+
+tmux new-sess -s dash-http -c /root/dashboard/frontend -d 'python -m http.server 80'
+tmux new-sess -s rtinfo-webui -c /root/dashboard/monitoring -d 'python -m http.server 81'
+
+tmux new-sess -s dash-main -c /root/dashboard/backend -d 'python dashboard.py'
+tmux new-sess -s persistance -c /root/dashboard/backend -d 'python persistance.py'
+tmux new-sess -s locator -c /root/dashboard/backend -d 'python location.py'
+tmux new-sess -s sensors -c /root/dashboard/backend -d 'python sensors.py'
+
+tmux new-sess -s rtinfo-websrv -c /root/dashboard/backend -d 'python dashboard-rtinfo.py'
+tmux new-sess -s rtinfo-fetcher -c /root/dashboard/backend -d 'python rtinfo.py'
