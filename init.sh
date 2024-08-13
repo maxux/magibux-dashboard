a30cc8f44113d096c73f0e9ed71f7ecca904d7c9
Maxime Daniel <root@maxux.net>
2024-08-13 02:39:02 +0200

    init: update script with latest file names

--- init.sh
+++ init.sh
@@ -6,7 +6,10 @@ tmux new-sess -s rtinfo-webui -c /root/dashboard/monitoring -d 'python -m http.s
 tmux new-sess -s dash-main -c /root/dashboard/backend -d 'python dashboard.py'
 tmux new-sess -s persistance -c /root/dashboard/backend -d 'python persistance.py'
 tmux new-sess -s locator -c /root/dashboard/backend -d 'python location.py'
-tmux new-sess -s sensors -c /root/dashboard/backend -d 'python sensors.py'
 
-tmux new-sess -s rtinfo-websrv -c /root/dashboard/backend -d 'python dashboard-rtinfo.py'
+tmux new-sess -s relays -c /root/dashboard/backend -d 'python relay.py'
+tmux new-sess -s temperatures -c /root/dashboard/backend -d 'python temperatures.py'
+tmux new-sess -s pressures -c /root/dashboard/backend -d 'python pressures.py'
+
 tmux new-sess -s rtinfo-fetcher -c /root/dashboard/backend -d 'python rtinfo.py'
+tmux new-sess -s camupdate -c /root/dashboard/backend -d 'python cameras.py'
