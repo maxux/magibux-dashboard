254b4cd29a755f650fc876e11969b1a4690739c8
Maxime Daniel <root@maxux.net>
2024-08-26 16:32:59 +0200

    init: improve tmux names and prettify script

--- init.sh
+++ init.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-tmux new-sess -s dash-http -c /root/dashboard/frontend -d 'python -m http.server 80'
-tmux new-sess -s rtinfo-webui -c /root/dashboard/monitoring -d 'python -m http.server 81'
+modules="dashboard persistance location relay temperatures pressures rtinfo cameras"
 
-tmux new-sess -s dash-main -c /root/dashboard/backend -d 'python dashboard.py'
-tmux new-sess -s persistance -c /root/dashboard/backend -d 'python persistance.py'
-tmux new-sess -s locator -c /root/dashboard/backend -d 'python location.py'
+for module in $modules; do
+    session="backend-${module}"
 
-tmux new-sess -s relays -c /root/dashboard/backend -d 'python relay.py'
-tmux new-sess -s temperatures -c /root/dashboard/backend -d 'python temperatures.py'
-tmux new-sess -s pressures -c /root/dashboard/backend -d 'python pressures.py'
+    tmux new-sess -s ${session} -d -c /root/dashboard/backend
+    tmux send-keys -t ${session} "python ${module}.py" ENTER
 
-tmux new-sess -s rtinfo-fetcher -c /root/dashboard/backend -d 'python rtinfo.py'
-tmux new-sess -s camupdate -c /root/dashboard/backend -d 'python cameras.py'
+    sleep 0.2
+done
+
+# tmux new-sess -s dash-main -c /root/dashboard/backend -d 'python dashboard.py'
+# tmux new-sess -s persistance -c /root/dashboard/backend -d 'python persistance.py'
+# tmux new-sess -s locator -c /root/dashboard/backend -d 'python location.py'
+# tmux new-sess -s relays -c /root/dashboard/backend -d 'python relay.py'
+# tmux new-sess -s temperatures -c /root/dashboard/backend -d 'python temperatures.py'
+# tmux new-sess -s pressures -c /root/dashboard/backend -d 'python pressures.py'
+# tmux new-sess -s rtinfo-fetcher -c /root/dashboard/backend -d 'python rtinfo.py'
+# tmux new-sess -s camupdate -c /root/dashboard/backend -d 'python cameras.py'
+# tmux new-sess -s camera -d -c /home/camera/camstream
