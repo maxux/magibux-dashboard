461007c093428934ee54177ff43e389c4d17bcb5
Maxime Daniel <root@maxux.net>
2024-09-16 17:54:48 +0200

    backend: tools: add dedicated class for terminal colors support

---
+++ backend/tools/colors.py
@@ -0,0 +1,11 @@
+class color:
+    black = '\033[30m'
+    red = '\033[1;31m'
+    green = '\033[1;32m'
+    yellow = '\033[1;33m'
+    blue = '\033[1;34m'
+    magenta = '\033[1;35m'
+    cyan = '\033[1;36m'
+    white = '\033[1;37m'
+
+    reset = '\033[0m'
